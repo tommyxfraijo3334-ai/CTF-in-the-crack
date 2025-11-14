c0eaa41bcabdd531910e5f7563d59240d21015d0
Vibesz <tommyxfraijo3334@gmail.com>
2025-11-13 17:57:12 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,47 @@
 #!/bin/bash
 
-# Update the box
+
 apt-get update -y
 
-# Install Apache
+
 apt-get install -y apache2
 
-# --- This is your vulnerability ---
 
-# 1. Create a "hidden" directory
+
+
 mkdir /var/www/html/this-is-a-secret-dir
 
-# 2. Create the user flag
+
 echo "flag{my_first_web_flag_123}" > /var/www/html/this-is-a-secret-dir/user.txt
 
-# 3. Create the robots.txt file to give the clue
+
 echo "User-agent: *" > /var/www/html/robots.txt
 echo "Disallow: /this-is-a-secret-dir/" >> /var/www/html/robots.txt
 
-# --- End of vulnerability ---
 
 # Restart Apache to make sure
 systemctl restart apache2
+
+# CTF Challenge 2: Privilege Escalation 
+
+
+echo "flag{gratz_u_are_root_9921}" > /root/root.txt
+
+chmod 600 /root/root.txt
+
+
+cp /bin/bash /usr/local/bin/get-root-shell
+
+chmod +s /usr/local/bin/get-root-shell
+
+
+apt-get install -y vsftpd
+
+
+sed -i 's/anonymous_enable=NO/anonymous_enable=YES/' /etc/vsftpd.conf
+
+
+echo "flag{ftp_is_fun_555}" > /srv/ftp/network-flag.txt
+
+
+systemctl restart vsftpd
